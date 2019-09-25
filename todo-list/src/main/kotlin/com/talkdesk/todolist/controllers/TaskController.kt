e88d20b3d61b11a6a486489b436009fcfffb648a
Nuno Adrego <nunoadrego.geral@gmail.com>
2019-09-25 15:19:26 +0100

    Delete task feature implemented

--- todo-list/src/main/kotlin/com/talkdesk/todolist/controllers/TaskController.kt
+++ todo-list/src/main/kotlin/com/talkdesk/todolist/controllers/TaskController.kt
@@ -2,6 +2,7 @@ package com.talkdesk.todolist.controllers
 
 import com.talkdesk.todolist.entities.Task
 import com.talkdesk.todolist.repositories.TaskRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -21,4 +22,20 @@ class TaskController(
     fun newTask(@RequestBody task : Task) : Task {
         return this.taskRepository.save(task)
     }
+
+    @DeleteMapping("/delete/{id}")
+    fun deleteTask(
+            @PathVariable id : String
+    ) : ResponseEntity<Task>  {
+        val task = this.taskRepository.findById(id)
+
+        if (task.isPresent){
+            this.taskRepository.deleteById(id)
+            return ResponseEntity.status(HttpStatus.OK).build()
+        }
+        return ResponseEntity.notFound().build()
+    }
+
+
+
 }
